f11053a2416d825e510d234a72b7270b4160dd90
priya <priya@santhila.co>
2024-10-23 23:41:17 +0530

    Added: PJ Aruvi Site

---
+++ js/main.js
@@ -0,0 +1,181 @@
+(function ($) {
+    "use strict";
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
+        });
+        console.log($videoSrc);
+
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
+
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
+    });
+
+
+    // Service carousel
+    $(".service-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:1
+            },
+            992:{
+                items:1
+            }
+        }
+    });
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
+
+    // Team carousel
+    $(".team-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        loop: true,
+    });
+
+    $(document).ready(function() {
+        $("#header").load("header.html");
+        $("#footer-placeholder").load("footer.html");
+    });   
+    $(document).ready(function() {
+        // Load the header and footer
+        $("#header").load("header.html", function() {
+            // Get the current page URL
+            var currentPath = window.location.pathname.split("/").pop();
+    
+            // If the URL is empty (e.g., root domain), default to index.html
+            if (currentPath === "") {
+                currentPath = "index.html";
+            }
+    
+            // Find the nav-link whose href matches the current page and add 'active' class
+            $('#header .navbar-nav a').each(function() {
+                var linkPath = $(this).attr('href').split("/").pop();
+    
+                // Check if the current page matches the link
+                if (linkPath === currentPath) {
+                    // Add 'active' class to the matching link
+                    $(this).addClass('active');
+    
+                    // Check if the active link is inside a dropdown
+                    if ($(this).closest('.dropdown-menu').length) {
+                        // Add 'active' class to the parent dropdown link
+                        $(this).closest('.dropdown').find('.nav-link').addClass('active');
+                    }
+                } else {
+                    $(this).removeClass('active');
+                }
+            });
+        });
+    
+        // Load the footer (if needed)
+        $("#footer-placeholder").load("footer.html");
+    });
+     
+})(jQuery);
+
